579aced880e2a29f9135f651cec986ccf6465e5d
Nick Hanselman <nickhans@econ1-144-227-dhcp.int.colorado.edu>
2018-09-09 11:46:21 -0600

    Init checkEvents

---
+++ checkEvents.py
@@ -0,0 +1,100 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import re
+import json
+
+def simple_get(url):
+  try:
+    with closing(get(url, stream=True)) as resp:
+      if is_good_response(resp):
+        return resp.content
+      else:
+        return None
+  
+  except RequestException as e:
+      log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+      return None
+
+def is_good_response(resp):
+  content_type = resp.headers['Content-Type'].lower()
+  return (resp.status_code == 200
+          and content_type is not None
+          and content_type.find('html') > -1)
+
+def log_error(e):
+  print(e)
+
+def parse_events(count, event_storage, raw_html):
+  html = BeautifulSoup(raw_html, 'html.parser')
+  events = html.find_all('a', href=re.compile(
+      "https://officenters.com/event/"))
+
+  for event in events:
+    if (event.string != '[Read More...]' and event.string != None and event.string != '(See all)'):
+      print('{0}. {1}'.format(count, event.string.strip()))
+      event_storage[count] = event.string.strip()
+      count += 1
+
+  return count, event_storage
+
+def retrieve_events(event_storage):
+  count = 1
+  raw_html = simple_get('https://officenters.com/events/')
+  count, event_storage = parse_events(count, event_storage, raw_html)
+  i = 2
+  prev_count = 0
+  while (prev_count != count):
+    prev_count = count
+    raw_html = simple_get(
+        'http://officenters.com/events/?action=tribe_photo&tribe_paged=' + str(i) + '&tribe_event_display=photo')
+    count, event_storage = parse_events(count, event_storage, raw_html)
+    i += 1
+  print("Retrieved All Events.")
+  return event_storage
+
+def save_event_data(event_storage):
+  event_json = json.dumps(event_storage)
+  with open('events.json', 'w') as outfile:
+    json.dump(event_json, outfile)
+
+def load_saved_data():
+  with open('events.json', 'r') as data_file:
+    saved_events = json.loads(data_file.read())
+  return saved_events
+
+def write_files(event_storage, previous_events):
+  f_current = open("current_events.txt", "w")
+  f_previous = open("previous_events.txt", "w")
+
+  for x, y in event_storage.items():
+    f_current.write('{0} {1}\n'.format(x, y))
+  for x, y in previous_events.items():
+    f_previous.write('{0} {1}\n'.format(x, y))
+
+  f_current.close()
+  f_previous.close()
+
+def main():
+  event_storage = {}
+  event_storage = retrieve_events(event_storage)
+  previous_events = {}
+  previous_events = json.loads(load_saved_data())
+
+  write_files(event_storage, previous_events)
+
+  while(True):
+    option = input('Save data as current events? y for yes, n for no: ')
+    if(option == 'y'):
+      save_event_data(event_storage)
+      print('Event data saved.')
+      break
+    elif(option == 'n'):
+      print("Data not saved.")
+      break
+    else:
+      print('Not a valid option')
+  
+
+main()
